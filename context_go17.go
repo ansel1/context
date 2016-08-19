9134873488b151ad11410cc227fac8047c23c259
Russ Egan <russ@safemonk.com>
2016-08-19 15:35:30 -0400

    initial

---
+++ context_go17.go
@@ -0,0 +1,41 @@
+// +build go1.7
+
+package context
+
+import (
+	"context"
+	"time"
+)
+
+var Canceled = context.Canceled
+
+var DeadlineExceeded = context.DeadlineExceeded
+
+func Background() Context {
+	return context.Background()
+}
+
+func TODO() Context {
+	return context.TODO()
+}
+
+type CancelFunc func()
+
+func WithCancel(parent Context) (ctx Context, cancel CancelFunc) {
+	ctx, f := context.WithCancel(parent)
+	return ctx, CancelFunc(f)
+}
+
+func WithDeadline(parent Context, deadline time.Time) (Context, CancelFunc) {
+	ctx, f := context.WithDeadline(parent, deadline)
+	return ctx, CancelFunc(f)
+}
+
+func WithTimeout(parent Context, timeout time.Duration) (Context, CancelFunc) {
+	ctx, f := context.WithTimeout(parent, timeout)
+	return ctx, CancelFunc(f)
+}
+
+func WithValue(parent Context, key, val interface{}) Context {
+	return context.WithValue(parent, key, val)
+}
